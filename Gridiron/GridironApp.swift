786e42fbf0acdca77389af8e0c0ba7a9ef7c72fd
RepmoD <david.brazeal@gmail.com>
2025-02-24 21:36:55 -0600

    Initial Commit

---
+++ Gridiron/GridironApp.swift
@@ -0,0 +1,32 @@
+//
+//  GridironApp.swift
+//  Gridiron
+//
+//  Created by David Brazeal on 2/24/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct GridironApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
